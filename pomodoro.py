cd0a82548c27d13837af738d28ee4e40f74748d4
JIVESH HAMAL <bravojivesh@gmail.com>
2024-02-25 21:01:35 +0530

    WIP, cycle is not working

--- pomodoro.py
+++ pomodoro.py
@@ -1,14 +1,35 @@
+# 25/5 for 3 times. On the fourth time 25 work and 20 min break.
+# 25/5.25/5.25/5.25/20------25/5.25/5.25/5.25/20
+#1,3,5,7== 25
+#2,4,6=5
+#8=20
 import time
 import tkinter as tk
+
+cycle_running=1
+eog=False
+
+work_time= 7
+mini_brk=2
+long_brk=5
 
 window1=tk.Tk()
 window1.config(width=500,height=500,bg="Lightyellow") #because we are using canvas this is no longer needed.
 
-def start_count(total_time):
+def start_count(time_in_sec):
     # print(total_time)
-    if total_time>=0:
-        canvas1.itemconfig(timer_digits, text=total_time)
-        window1.after(1000,start_count,total_time-1) #new function to learn and recursion.
+    global cycle_running
+    if time_in_sec>=0:
+        print ("ttttt")
+        min = "{:02d}".format(time_in_sec // 60) # i did it without using time module :)
+        sec = "{:02d}".format(time_in_sec % 60)
+        new_time = min + ":" + sec
+        # print(new_time)
+        canvas1.itemconfig(timer_digits, text=new_time)
+        window1.after(1000, start_count, time_in_sec - 1) #new function to learn and recursion.
+        print (type(time_in_sec))
+    elif time_in_sec<0:
+        cycle_running += 1
 
 
 #==============================================================================
@@ -23,8 +44,17 @@ def start_count(total_time):
 #==============================================================================
 
 def start():
-    start_count(5)
-
+    global eog, cycle_running, work_time, mini_brk, long_brk
+    while eog==False:
+        if cycle_running % 2 == 1 and cycle_running<8:
+            start_count(work_time)
+            print (cycle_running % 2,'asdas')
+            time.sleep(2)
+        elif cycle_running %2 ==0 and cycle_running<8:
+            start_count(mini_brk)
+            print ("2121")
+        else:
+            start_count(long_brk)
 
 photo1=tk.PhotoImage(file="tomato.png")
 canvas1= tk.Canvas(width=500,height=500,bg="Lightyellow",highlightthickness=0) #without highlightthickness you see a white border
